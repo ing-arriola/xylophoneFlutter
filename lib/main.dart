a642b7c4baa0778cb4bdb7104bd359d129a25076
ing-arriola <ingeniero.arriola@gmail.com>
2020-12-24 19:34:13 -0600

    now playing souds through a function

--- lib/main.dart
+++ lib/main.dart
@@ -5,71 +5,85 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  // call this method when desired
-                  player.play('note1.wav');
-                },
-                child: Text(
-                  'BTN 1',
-                  style: TextStyle(fontSize: 20),
+              Container(
+                height: 100,
+                color: Colors.red,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: Text(
+                    'BTN 1',
+                    style: TextStyle(fontSize: 20),
+                  ),
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  // call this method when desired
-                  player.play('note2.wav');
-                },
-                child: Text('BTN 2'),
+              Container(
+                color: Colors.orange,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  child: Text('BTN 2'),
+                ),
               ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  // call this method when desired
-                  player.play('note3.wav');
-                },
-                child: Text('BTN 3'),
+              Container(
+                color: Colors.yellow,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  child: Text('BTN 3'),
+                ),
               ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  // call this method when desired
-                  player.play('note4.wav');
-                },
-                child: Text('BTN 4'),
+              Container(
+                color: Colors.green,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  child: Text('BTN 4'),
+                ),
               ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  // call this method when desired
-                  player.play('note5.wav');
-                },
-                child: Text('BTN 5'),
+              Container(
+                color: Colors.teal,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  child: Text('BTN 5'),
+                ),
               ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  // call this method when desired
-                  player.play('note6.wav');
-                },
-                child: Text('BTN 6'),
+              Container(
+                color: Colors.blue,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  child: Text('BTN 6'),
+                ),
               ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  // call this method when desired
-                  player.play('note7.wav');
-                },
-                child: Text('BTN 7'),
+              Container(
+                color: Colors.purple,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: Text('BTN 7'),
+                ),
               ),
             ],
           ),
